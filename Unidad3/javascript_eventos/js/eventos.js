fcda6baf167e44204c7f94cd14e3da075d1d6d94
Carlos Madrid <Carlos Madrid>
2025-10-28 12:52:38 -0700

    Clase martes 28 antes de la tarea

---
+++ Unidad3/javascript_eventos/js/eventos.js
@@ -0,0 +1,40 @@
+let elemento=document.querySelector("#elemento");
+let agregar=document.querySelector("#agregar");
+let lista=document.querySelector("#listadinamica");
+elemento.focus();
+
+agregar.addEventListener("click",() =>{
+    if(elemento.value.trim() != ""){
+        let nuevoelemento=document.createElement("li");
+        nuevoelemento.innerText=elemento.value;
+        lista.appendChild(nuevoelemento);
+        // elemento.value = elemento.value.trim();
+        elemento.value = "";
+        elemento.focus();
+    }
+});
+
+elemento.addEventListener("keydown",(e) => {
+    if(e.key == "Enter"){
+        agregar.click();
+    }
+});
+
+//Arrastrar y soltar
+let arrastra=document.querySelector("#arrastra");
+let zonasoltar=document.getElementById("zonasoltar");
+
+arrastra.addEventListener("dragstart",function(e){
+    this.style.border="5px solid red";
+});
+
+zonasoltar.addEventListener("dragover",function(e){
+    //zonasoltar olvida que no se pueden
+    //arrastrar elementos sobre el
+    event.preventDefault();
+
+});
+zonasoltar.addEventListener("drop", function(e){
+    arrastra.style.border="none";
+    this.append(arrastra);
+});
