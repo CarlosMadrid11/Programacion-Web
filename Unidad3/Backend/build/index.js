4aceeb350b7721691b3588af362bbfae9601d67b
Carlos Madrid <b4rc4drid@gmail.com>
2025-11-13 11:57:46 -0700

    Actu antes de la clase del jueves

--- Unidad3/Backend/build/index.js
+++ Unidad3/Backend/build/index.js
@@ -1,19 +1,18 @@
-// API --- BACKEND
-// NODEJS = EXPRESS
-import express from 'express';
+import express, { Router } from 'express';
 // Creamos la aplicacion a traves del paquete express
 // y llamamos a su constructor
 const app = express();
 // Todo lo que devuelve al usuario es en formato JSON
 app.use(express.json());
+// Configurar rutas para el acceso a personal
+import personalRutas from './routes/personalRutas.js';
+// Activar la ruta base de personal 
+app.use('/api/personal', personalRutas);
 // puerto para escucahar las peticiones del frontend
 const PUERTO = 3001;
-// RUTA
-app.get('/hola', (_req, res) => {
-    let fecha = new Date().toLocaleDateString();
-    res.send('mundo con la fecha ' + fecha);
-});
 // encendemos el servidor y lo ponemos en modo escucha
 app.listen(PUERTO, () => {
     console.log(`Servidor escuchando en el puerto ${PUERTO}`);
 });
+export default Router;
+// extencion para tener los json bonitos: JSON FORMATTER
---
+++ Unidad3/Backend/build/services/personalServices.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise';
+const conexion = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'Api_Proweb'
+});
+export const obtienePersonal = async () => {
+    try {
+        const [results] = await conexion.query('SELECT * FROM personal');
+        return results;
+    }
+    catch (error) {
+        return { error: "No se puede obtener la lista del personal" };
+    }
+};
